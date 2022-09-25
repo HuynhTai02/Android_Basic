a1533ef0f1fa619cec8517ddcdce73c1108df780
HuynhTai02 <hnt8102@gmail.com>
2022-09-25 10:02:48 +0700

    W1

---
+++ Demo_UI/app/src/main/java/com/tai/demo_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tai.demo_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_m000_splash);
+    }
+}
