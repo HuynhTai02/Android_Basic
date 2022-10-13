6e545eb16f40765603c01ae8cb504876d5098460
HuynhTai02 <hnt8102@gmail.com>
2022-10-13 22:48:56 +0700

    W4

---
+++ Restaurant_Booking_Uikit/app/src/main/java/com/tai/practice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tai.practice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_m005_main);
+    }
+}
