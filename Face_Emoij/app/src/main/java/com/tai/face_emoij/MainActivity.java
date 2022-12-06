f86d974f0ef6dd3f1d844d4704c4ba019c0617b7
HuynhTai02 <hnt8102@gmail.com>
2022-12-06 09:38:01 +0700

    Custom_Toast-Dialog-AlertDialog

--- Face_Emoij/app/src/main/java/com/tai/face_emoij/MainActivity.java
+++ Face_Emoij/app/src/main/java/com/tai/face_emoij/MainActivity.java
@@ -1,14 +1,107 @@
 package com.tai.face_emoij;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import android.os.Bundle;
+import com.tai.face_emoij.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends BaseAct<ActivityMainBinding> {
+
+    private AlertDialog alert;
+    private CloseDialog dialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void initViews() {
+        binding.imgFun.setOnClickListener(this);
+        binding.imgHappy.setOnClickListener(this);
+        binding.imgHurt.setOnClickListener(this);
+        binding.imgInLove.setOnClickListener(this);
+        binding.imgMakeFun.setOnClickListener(this);
+        binding.imgSick.setOnClickListener(this);
+        binding.imgSulk.setOnClickListener(this);
+        binding.imgWhoa.setOnClickListener(this);
+        binding.imgYahoo.setOnClickListener(this);
+    }
+
+    @Override
+    protected void clickView(View view) {
+        String text = view.getContentDescription().toString();
+        Drawable drawable = ((ImageView) view).getDrawable();
+
+        showToast(text, drawable);
+    }
+
+    private void showToast(String text, Drawable drawable) {
+        Toast toast = new Toast(this);
+
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_custom_toast_face, null);
+        ImageView ivFace = view.findViewById(R.id.img_face);
+        TextView tvFace = view.findViewById(R.id.tv_face);
+
+        tvFace.setText(text);
+        ivFace.setImageDrawable(drawable);
+        toast.setView(view);
+
+        int wScreen = getResources().getDisplayMetrics().widthPixels;
+        int hScreen = getResources().getDisplayMetrics().heightPixels;
+        Random rd = new Random();
+        int x = rd.nextInt(wScreen);
+        int y = rd.nextInt(hScreen);
+        toast.setGravity(Gravity.TOP | Gravity.START, x, y);
+
+        toast.show();
+    }
+
+    @Override
+    protected ActivityMainBinding initViewBinding() {
+        return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public void onBackPressed() {
+//      showAlertDialog();
+
+        showCloseDialog();
+    }
+
+    private void showCloseDialog() {
+        if (dialog == null) {
+            dialog = new CloseDialog(this);
+            dialog.setOnCloseApp(this::finish);
+        }
+
+        dialog.show();
+    }
+
+    private void showAlertDialog() {
+        if (alert == null) {
+            alert = new AlertDialog.Builder(this).create();
+
+            //Không thể kích sang bên ngoài
+            alert.setCanceledOnTouchOutside(false);
+            //Ấn backpress k tắt đc alertdialog
+            alert.setCancelable(false);
+
+            alert.setTitle("Alert");
+            alert.setMessage("Close app?");
+
+            alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog, which) -> {
+                // Xử lý khi người dùng click vào OK
+                super.onBackPressed();
+            });
+
+            alert.setButton(AlertDialog.BUTTON_NEGATIVE, "DON'T", (dialog, which) -> {
+                // Do nothing
+            });
+        }
+        alert.show();
     }
 }
